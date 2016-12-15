9e0f7f607c23a264c49f05271206ee585235f9c9
abraxasrex <jbreasor@gmail.com>
2016-12-14 20:35:27 -0800

    gets working, now post, need to build api

--- client/controllers/mainController.ts
+++ client/controllers/mainController.ts
@@ -1,5 +1,23 @@
 class mainController {
-    public message = 'Hello from the about page!';
+    public message = `Hello from the controller!`;
+    public items;
+    public item;
+    public remoteAPI = 'https://jsonplaceholder.typicode.com/posts';
+    public localAPI = 'file:///home/jona/code_demos/ng-week-2/client/index.html/myItems';
+    public postItem(){
+      this.$http.post(this.localAPI, this.item).then(()=>{
+        console.log(`yeah.`);
+      }).catch((err)=>{
+        console.log(err);
+      });
+    }
+    constructor(private $http: ng.IHttpService){
+      this.$http.get(this.remoteAPI).then((res)=>{
+        console.log('res: ', res);
+        this.items = res.data;
+      }).catch((err)=>{
+        console.log(err);
+      });
+    }
 }
-
 angular.module('exampleApp').controller('mainController', mainController);
