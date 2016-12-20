e27b9428c218526ca751a3d8bd44f33c35398bb6
abraxasrex <jbreasor@gmail.com>
2016-12-20 14:13:38 -0800

    day1 complete

--- client/app.ts
+++ client/app.ts
@@ -1,2 +1,17 @@
-//TODO: why wrap app and controllers in namespace?
-  angular.module('exampleApp', []);
+angular.module('exampleApp', []);
+
+class mainController {
+  message="Hello World!";
+  groceries=[
+    {name: 'apples', price: 2},
+    {name: 'bananas', price: 3},
+    {name: 'chard', price: 100} ] ;
+  printMe(item){
+    console.log('I am: ', item);
+  }
+  constructor(){
+
+  }
+}
+
+angular.module('exampleApp').controller('mainController', mainController);
